107ba8a199cbd8ddee0f463e7248a22f58c54760
hasukjun <ydha93@hanmail.net>
2016-09-10 22:40:29 +0900

    second changed version

--- haha/src/haha/hahaha.java
+++ haha/src/haha/hahaha.java
@@ -5,6 +5,7 @@ public class hahaha {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello java");
+System.out.println("Hello java2");
 //first changed version
 //second changed version
 	}
