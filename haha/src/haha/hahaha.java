626be52c29c93abca84e808fe81df9f7a68fd84d
hasukjun <ydha93@hanmail.net>
2016-09-11 10:23:07 +0900

    first merge

--- haha/src/haha/hahaha.java
+++ haha/src/haha/hahaha.java
@@ -5,11 +5,7 @@ public class hahaha {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello java");
-<<<<<<< HEAD
-System.out.println("Hello java2");
-=======
 System.out.println("Hello java3");
->>>>>>> refs/heads/first_branch
 //first changed version
 //second changed version
 	}
